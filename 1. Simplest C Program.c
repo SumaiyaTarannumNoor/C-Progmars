823ba27b64ed996221adbcd05f87e6e6bba2f033
Sumaiya Tarannum Noor <50949858+SumaiyaTarannumNoor@users.noreply.github.com>
2021-03-02 11:07:28 +0600

    Add files via upload

---
+++ 1. Simplest C Program.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(void)
+{
+printf ("This is a short C Program");
+return 0;
+}
---
+++ 5. Asks to input an integer and a floating point number..c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num;
+    float f;
+
+    printf ("Enter an integer: ");
+    scanf ("%d", &num);
+
+    printf ("Enter a floating point number: ");
+    scanf ("%f", &f);
+
+    return 0;
+}
